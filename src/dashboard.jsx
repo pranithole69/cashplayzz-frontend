6b51523f431de30612a068cd4cce955926df8491
Pranit Hole <pranithole1@gmail.com>
2025-07-02 00:13:26 +0530

    Temp rename to force Vercel to reset casing

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./dashboard.css";
+import './temp.css';
 import { FaWallet, FaBars, FaTimes } from "react-icons/fa";
 import { GiTwoCoins } from "react-icons/gi";
 
